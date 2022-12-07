88324c05b7e4ed7ce1fbeda85f548e02ba91c2fe
BrickHouse00 <bthornton05@gmail.com>
2022-12-06 20:26:16 -0500

    Update build information

---
+++ src/main/java/net/brickhouse/ordersofmagnitude/mixins/MixinBasePressurePlateBlock.java
@@ -0,0 +1,24 @@
+package net.brickhouse.ordersofmagnitude.mixins;
+
+import net.brickhouse.ordersofmagnitude.sizechange.SizeUtility;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BasePressurePlateBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BasePressurePlateBlock.class)
+public abstract class MixinBasePressurePlateBlock {
+
+    @Inject(method="checkPressed", at=@At("HEAD"), cancellable = true)
+    private void onCheckPressed(Entity pEntity, Level pLevel, BlockPos pPos, BlockState pState, int pCurrentSignal, CallbackInfo ci){
+        if(pEntity != null && SizeUtility.getScale(pEntity) <= 0.25){       //check if entity is null.  Will be null when entity leaves the pressure plate
+            ci.cancel();
+        }
+    }
+
+}
