d2f16005dc46025cbb606de25e56a46a409d4272
BrickHouse00 <bthornton05@gmail.com>
2022-12-08 14:07:27 -0500

    Advancements and language files

--- src/main/java/net/brickhouse/ordersofmagnitude/advancements/ModCriteriaTriggers.java
+++ src/main/java/net/brickhouse/ordersofmagnitude/advancements/ModCriteriaTriggers.java
@@ -1,7 +1,5 @@
 package net.brickhouse.ordersofmagnitude.advancements;
 
-import net.minecraft.advancements.CriterionTrigger;
-
 public class ModCriteriaTriggers {
     public final static ChangeSizeTrigger CHANGE_SIZE = new ChangeSizeTrigger();
 
